0d0f2bd02d1926f1cba5300c3b6d469a7537dd25
stashukvadim <stashukvadim@gmail.com>
2016-10-18 21:03:04 +0300

    Add correct concurrency test. Add simple tests. Update junit version in pom.xml.

--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -4,6 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class WordCounterTest extends Assert {
 
     private static final WordCounterServiceImpl wcs = WordCounterServiceImpl.getInstance();
@@ -72,5 +76,42 @@ public class WordCounterTest extends Assert {
         int actualCount = wcs.getWordCount("venja");
         Assert.assertEquals(threadCount, actualCount);
     }
+    /*You should always start with these primitive tests before writing complicated tests*/
+    @Test
+    public void shouldReturnZeroWhenWordWasNeverAdded() {
+        int wordThatWasNeverAddedCount = wcs.getWordCount("word_that_was_never_added");
 
+        assertEquals(0, wordThatWasNeverAddedCount);
+    }
+
+    @Test
+    public void shouldReturnOneWhenWordWasAddedOneTime() {
+        final String wordToAddOneTime = "wordToAddOneTime";
+        wcs.sendWord(wordToAddOneTime);
+        int wordToAddOneTimeCount = wcs.getWordCount(wordToAddOneTime);
+
+        assertEquals(1, wordToAddOneTimeCount);
+    }
+
+    @Test
+    public void correctConcurrencyTest() throws InterruptedException {
+        final int n = 1000;
+        final int numberOfThreads = 1000;
+        final String word = "word";
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+        Runnable addWordNTimesTask = new Runnable() {
+            public void run() {
+                for (int i = 0; i < n; i++) {
+                    wcs.sendWord(word);
+                }
+            }
+        };
+        for (int i = 0; i < numberOfThreads; i++) {
+            executorService.submit(addWordNTimesTask);
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+
+        assertEquals(n * numberOfThreads, wcs.getWordCount(word));
+    }
 }
